dd7df44807dc50a611263b4c70e21b1afe9b9ba2
Abi-CP <c.p.abishek@gmail.com>
2024-01-10 23:21:47 +0530

    minor fixes

--- index.js
+++ index.js
@@ -1,104 +1,114 @@
-const body = document.querySelector("body");
+const body = document.querySelector('body')
 
 // Navbar background change on scroll
-const nav = document.querySelector(".nav-container");
-var navActiveFlag = 0;
-let pausedNav = false;
-document.addEventListener("scroll", () => {
-  if (navActiveFlag < 1){
-    console.log("scroll")
-    activateNav();
+const nav = document.querySelector('.nav-container')
+var navActiveFlag = 0
+let pausedNav = false
+document.addEventListener('scroll', () => {
+  if (navActiveFlag < 1) {
+    console.log('scroll')
+    activateNav()
   }
-});
+})
 
-// nav.addEventListener("hover")
-document.addEventListener("scrollend", () => {
-  console.log("scrollend");
-  setTimeout(removeNav, 1500);
-});
+document.addEventListener('scrollend', () => {
+  console.log('scrollend')
+  setTimeout(removeNav, 1000)
+})
 
-
-let startX;
-
-body.addEventListener('touchstart', handleTouchStart);
-body.addEventListener('touchend', handleTouchEnd);
+let windowWidth = window.innerWidth
+let initialX = null
 
 function handleTouchStart(event) {
-  startX = event.touches[0].clientX;
+  initialX = event.touches[0].clientX
 }
 
-function handleTouchEnd(event) {
-  const endX = event.changedTouches[0].clientX;
-  const deltaX = endX - startX;
-
-  const swipeThreshold = 50;
-  if (deltaX < -swipeThreshold) {
-    console.log('Swiped left!');
-    activateNav();
-    setTimeout(removeNav, 1500);
+function handleTouchMove(event) {
+  if (initialX === null) {
+    return
   }
+
+  const currentX = event.touches[0].clientX
+  const diffX = initialX - currentX
+  console.log(initialX + ',' + currentX)
+
+  if (diffX > 0) {
+    console.log('Swipe left')
+    if (initialX > windowWidth * 0.7) {
+      activateNav()
+      setTimeout(removeNav, 2500)
+    }
+  } else if (diffX < 0) {
+    console.log('Swipe right')
+    if (navActiveFlag > 0) {
+      removeNav()
+    }
+  }
+
+  initialX = null
 }
+
+body.addEventListener('touchstart', handleTouchStart)
+body.addEventListener('touchmove', handleTouchMove)
 
 //Theme
-const themeButton = document.querySelector(".theme-button");
-const containerMainClassList = document.querySelector(".container-main").classList;
-const themeButtonClassList = themeButton.classList;
+const themeButton = document.querySelector('.theme-button')
+const containerMainClassList = document.querySelector('.container-main').classList
+const themeButtonClassList = themeButton.classList
 // Theme switch
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-  activateDark();
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  activateDark()
 }
 
-themeButton.addEventListener("click", () => {
-  themeChange();
-});
+themeButton.addEventListener('click', () => {
+  themeChange()
+})
 
 function themeChange() {
-  containerMainClassList.contains("dark") ? removeDark() : activateDark();
+  containerMainClassList.contains('dark') ? removeDark() : activateDark()
 }
 
 function activateDark() {
-  themeButtonClassList.remove("bxs-moon");
-  containerMainClassList.toggle("dark");
-  themeButtonClassList.add("bx-sun");
-  document.body.style.backgroundColor = "black";
+  themeButtonClassList.remove('bxs-moon')
+  containerMainClassList.toggle('dark')
+  themeButtonClassList.add('bx-sun')
+  document.body.style.backgroundColor = 'black'
 }
 
 function removeDark() {
-  themeButtonClassList.remove("bx-sun");
-  containerMainClassList.toggle("dark");
-  themeButtonClassList.add("bxs-moon");
-  console.log("removeDark");
-  body.style.backgroundColor = "white"; 
+  themeButtonClassList.remove('bx-sun')
+  containerMainClassList.toggle('dark')
+  themeButtonClassList.add('bxs-moon')
+  console.log('removeDark')
+  body.style.backgroundColor = 'white'
 }
 
-function activateNav(){
-  nav.classList.add("active");
-  navActiveFlag++;
+function activateNav() {
+  nav.classList.add('active')
+  navActiveFlag = 1
 }
 
-function pauseNav(){
-  pausedNav = true;
-  console.log("pNav");
+function pauseNav() {
+  pausedNav = true
+  console.log('pNav')
 }
 
-function removePauseNav(){
-  pausedNav = false;
-  console.log("pRemNav");
-  setTimeout(removeNav, 500);
+function removePauseNav() {
+  pausedNav = false
+  console.log('pRemNav')
+  setTimeout(removeNav, 500)
 }
 
-function removeNav(){
-  if (pausedNav == false){
-    console.log("rNav");
-    nav.classList.remove("active");
-    navActiveFlag--;
+function removeNav() {
+  if (pausedNav == false) {
+    console.log('rNav')
+    nav.classList.remove('active')
+    navActiveFlag = 0
   }
-  
 }
 
 // Routing
 
-
 // Check for full load
-console.log('js loaded successfully');
-removeNav();
+console.log('js loaded successfully')
+removeNav()
